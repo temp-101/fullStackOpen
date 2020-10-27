84d3e0e0ec2b90f26219f93fe53fc1e7b602947e
Abishek C <abishek.c101@gmail.com>
2020-10-27 05:42:28 +0530

    1.11

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,17 +1,72 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+const Button = ({handleClick, text}) => (
+  <button onClick={handleClick}>{text}</button>
+)
+
+const Feedback = (props) => {
+  return(
+    <>
+    <Button handleClick={() => props.setGood(props.good+1)} text="Good" />
+    <Button handleClick={() => props.setNeutral(props.neutral+1)} text="Neutral" />
+    <Button handleClick={() => props.setBad(props.bad+1)} text="Bad" />
+    </>
+  )
+}
+
+const Statistic = ({text, value}) => {
+  return(
+    <tr>
+      <th>{text}</th>
+      <th>{value}</th>
+    </tr>
+  )
+}
+  
+
+
+const Statistics = ({good, neutral, bad}) => {
+  if(good+bad+neutral === 0){
+    return(
+      <>
+      <p>No feedback given</p>
+      </>
+    )
+  }
+  else {
+    return(
+      <table>
+        <tbody>
+          <Statistic text="good" value={good} />
+          <Statistic text="neutral" value={neutral} />
+          <Statistic text="bad" value={bad} />
+          <Statistic text="all" value={good+neutral+bad} />
+          <Statistic text="average" value={(good-bad)/(good+bad+neutral)} />
+          <Statistic text="positive" value={good/(good+bad+neutral)*100} />
+        </tbody>
+      </table>
+    )
+  }
+}
+
+
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h1>Give Feedback</h1>
+      <Feedback good={good} setGood={setGood} neutral={neutral} setNeutral={setNeutral} bad={bad} setBad={setBad}/>
+      <h1>Statistics</h1>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, 
   document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
