96d184e5d8e33d2eff7b2847a439eb816b2490d9
Abishek C <abishek.c101@gmail.com>
2020-11-10 15:30:51 +0530

    2.11

--- part2/phonebook/src/app.js
+++ part2/phonebook/src/app.js
@@ -1,73 +1,81 @@
-import React, { useState } from 'react'
-import Persons from './components/person'
-import Filter from './components/fliter'
-import PersonForm from './components/personForm'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Persons from "./components/person";
+import Filter from "./components/fliter";
+import PersonForm from "./components/personForm";
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
-  const [ newName, setNewName ] = useState('')
-  const [ newNumber, setNewNumber ] = useState('')
-  const [ newSearch, setNewSearch ] = useState('')
+	const [persons, setPersons] = useState([]);
+	const [newName, setNewName] = useState("");
+	const [newNumber, setNewNumber] = useState("");
+	const [newSearch, setNewSearch] = useState("");
 
-  const addPerson = (event) => {
-      event.preventDefault()
-      const newPerson = {
-          name: newName,
-          number: newNumber,
-      }
+	useEffect(() => {
+		console.log("effect");
+		axios.get("http://localhost:3001/persons").then((response) => {
+			console.log("promise fulfilled");
+			setPersons(response.data);
+		});
+	}, []);
+	console.log("render", persons.length, "notes");
 
-      function duplicate(people) {
-          return people.name === newPerson.name;
-      }
+	const addPerson = (event) => {
+		event.preventDefault();
+		const newPerson = {
+			name: newName,
+			number: newNumber,
+		};
 
-      if(persons.find(duplicate)){
-          alert(`${newName} is already added to phonebook`)
-      }
-      else {
-        setPersons(persons.concat(newPerson))
-        setNewName('')
-        setNewNumber('')
-      }
-  }
+		function duplicate(people) {
+			return people.name === newPerson.name;
+		}
 
-  const handleNewName = (event) => {
-      setNewName(event.target.value)
-  }
+		if (persons.find(duplicate)) {
+			alert(`${newName} is already added to phonebook`);
+		} else {
+			setPersons(persons.concat(newPerson));
+			setNewName("");
+			setNewNumber("");
+		}
+	};
 
-  const handleNewNumber = (event) => {
-      setNewNumber(event.target.value)
-  }
+	const handleNewName = (event) => {
+		setNewName(event.target.value);
+	};
 
-  const handleSearchChange = (event) => {
-      console.log(newSearch)
-      setNewSearch(event.target.value)
+	const handleNewNumber = (event) => {
+		setNewNumber(event.target.value);
+	};
 
-  }
+	const handleSearchChange = (event) => {
+		console.log(newSearch);
+		setNewSearch(event.target.value);
+	};
 
-  const personsToShow = newSearch
-    ?  persons.filter((person) =>
-    person.name.toLowerCase().includes(newSearch.toLowerCase()))
-    : persons;
+	const personsToShow = newSearch
+		? persons.filter((person) =>
+				person.name.toLowerCase().includes(newSearch.toLowerCase())
+		  )
+		: persons;
 
-  return (
-    <div>
-      <h2>Phonebook</h2>
-      <Filter newSearch={newSearch} handleSearchChange={handleSearchChange} />
-      <h2>add a new</h2>
-      <PersonForm 
-      addPerson={addPerson} newName={newName} newNumber={newNumber} 
-      handleNewName={handleNewName} handleNewNumber={handleNewNumber} />
-      <h2>Numbers</h2>
-      {personsToShow.map(person =>
-        <Persons key={person.name} person={person} />
-        )}
-    </div>
-  )
-}
+	return (
+		<div>
+			<h2>Phonebook</h2>
+			<Filter newSearch={newSearch} handleSearchChange={handleSearchChange} />
+			<h2>add a new</h2>
+			<PersonForm
+				addPerson={addPerson}
+				newName={newName}
+				newNumber={newNumber}
+				handleNewName={handleNewName}
+				handleNewNumber={handleNewNumber}
+			/>
+			<h2>Numbers</h2>
+			{personsToShow.map((person) => (
+				<Persons key={person.name} person={person} />
+			))}
+		</div>
+	);
+};
 
-export default App
+export default App;
